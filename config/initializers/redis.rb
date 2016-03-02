5fae96cc09259b152f180b9284ea8727fcfa383a
jallous1 <jallous1@yahoo.fr>
2016-03-02 11:08:22 +0000

    02II16

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,29 +1,29 @@
 $redis = Redis.new
 
-#     DATA = JSON.parse(File.read("#{Rails.root}/public/postal_code_fr.json"))
+    DATA = JSON.parse(File.read("#{Rails.root}/public/postal_code_fr.json"))
 
-#     DATA["cp_autocomplete"].each.with_index(1) do |f, index|
-#       $redis.hset 'postal-data', index, f.to_json
-#     end
+    DATA["cp_autocomplete"].each.with_index(1) do |f, index|
+      $redis.hset 'postal-data', index, f.to_json
+    end
 
-#     DATA["cp_autocomplete"].each.with_index(1) do |f, index|
-#       (2..4).each do |n|
-#         prefix = (f['CP'][0..n]).to_s
-#         $redis.zadd "postal:#{prefix}", 0, index
-#       end
-#     end
+    DATA["cp_autocomplete"].each.with_index(1) do |f, index|
+      (2..4).each do |n|
+        prefix = (f['CP'][0..n]).to_s
+        $redis.zadd "postal:#{prefix}", 0, index
+      end
+    end
 
-#     DATA["cp_autocomplete"].each do |f|
-#       f["CITY_PAR"] = f['VILLE'].parameterize
-#     end
+    DATA["cp_autocomplete"].each do |f|
+      f["CITY_PAR"] = f['VILLE'].parameterize
+    end
 
-#     DATA["cp_autocomplete"].each.with_index(1) do |f, index|
-#       (2..((f["CITY_PAR"].length)-1)).each do |n|
-#         prefix = (f['CITY_PAR'][0..n]).to_s.downcase
-#         $redis.zadd "city:#{prefix}", 0, index
-#       end
-#     end
-# DATA = nil
+    DATA["cp_autocomplete"].each.with_index(1) do |f, index|
+      (2..((f["CITY_PAR"].length)-1)).each do |n|
+        prefix = (f['CITY_PAR'][0..n]).to_s.downcase
+        $redis.zadd "city:#{prefix}", 0, index
+      end
+    end
+DATA = nil
 
 
 
